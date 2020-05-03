cf433cd82d45d6b63aa5867a8bc426db8d0a176d
Luiz Ferreira <luiz.ferreira@outlook.com>
2020-05-03 19:43:52 -0300

    Typeorm initial config

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
+import 'reflect-metadata';
+
 import express from 'express';
 import routes from './routes';
 
@@ -8,6 +10,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(3334, () => {
+app.listen(3333, () => {
   console.log('🚀 Server started on port 3333!');
 });
--- src/services/CreateAppointmentService.ts
+++ src/services/CreateAppointmentService.ts
@@ -1,12 +1,8 @@
 import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
-
-/**
- * [x] Recebimento das informações
- * [/] Tratativa de erros e exceções
- * [/] Acesso ao repositório
- */
 
 interface Request {
   provider: string;
@@ -14,16 +10,12 @@ interface Request {
 }
 
 class CreateAppointmentService {
-  private appointmentsRepository: AppointmentsRepository;
+  public async execute({ provider, date }: Request): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
 
-  constructor(appointmentsRepository: AppointmentsRepository) {
-    this.appointmentsRepository = appointmentsRepository;
-  }
-
-  public execute({ provider, date }: Request): Appointment {
     const appointmentDate = startOfHour(date);
 
-    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(
+    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
       appointmentDate,
     );
 
@@ -31,10 +23,12 @@ class CreateAppointmentService {
       throw Error('This appointment is already booked');
     }
 
-    const appointment = this.appointmentsRepository.create({
+    const appointment = appointmentsRepository.create({
       provider,
       date: appointmentDate,
     });
+
+    await appointmentsRepository.save(appointment);
 
     return appointment;
   }
