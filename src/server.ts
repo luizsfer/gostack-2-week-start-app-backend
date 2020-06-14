6bb42f8d5f943a488281be107a39e93f6bc19d55
Luiz Ferreira <luiz.ferreira@clicksign.com>
2020-06-14 12:12:09 -0300

    Add cors config

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 import uploadConfig from './config/upload';
 
@@ -8,6 +9,7 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
